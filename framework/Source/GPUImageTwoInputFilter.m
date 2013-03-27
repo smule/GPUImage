9345618a3e37c98d37789720a11213ebc4975de6
Ian Simon <biriba@gmail.com>
2013-03-26 18:50:09 -0700

    2nd input to two input filter can also set size

--- framework/Source/GPUImageTwoInputFilter.m
+++ framework/Source/GPUImageTwoInputFilter.m
@@ -144,15 +144,16 @@ NSString *const kGPUImageTwoInputTextureVertexShaderString = SHADER_STRING
 
 - (void)setInputSize:(CGSize)newSize atIndex:(NSInteger)textureIndex;
 {
-    if (textureIndex == 0)
-    {
+    // ian: WHY?
+    //if (textureIndex == 0)
+    //{
         [super setInputSize:newSize atIndex:textureIndex];
         
         if (CGSizeEqualToSize(newSize, CGSizeZero))
         {
             hasSetFirstTexture = NO;
         }
-    }
+    //}
 }
 
 - (void)setInputRotation:(GPUImageRotationMode)newInputRotation atIndex:(NSInteger)textureIndex;
