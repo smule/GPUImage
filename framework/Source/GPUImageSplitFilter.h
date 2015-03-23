e53da3f02163aa4f08dc339dd8af9e1a09f85009
Michael Harville <michael.harville@smule.com>
2015-03-23 13:53:37 -0700

    support swipable video fx in Sing

---
+++ framework/Source/GPUImageSplitFilter.h
@@ -0,0 +1,15 @@
+//
+//  GPUImageSplitFilter.h
+//  Pods
+//
+//  Created by Michael Harville on 3/19/15.
+//
+//
+
+#import "GPUImageTwoInputFilter.h"
+
+@interface GPUImageSplitFilter : GPUImageTwoInputFilter
+
+- (void)setOffset:(CGFloat)offset;
+
+@end
