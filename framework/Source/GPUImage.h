2337c6b99083b40beea08800900efc354e9b76f7
John Shimmin <john@smule.com>
2015-03-27 18:08:39 -0700

    Make GPUImage.h play nicely on macosx

--- framework/Source/GPUImage.h
+++ framework/Source/GPUImage.h
@@ -4,21 +4,24 @@
 #import "GPUImageContext.h"
 #import "GPUImageOutput.h"
 #import "GPUImageView.h"
-#import "GPUImageVideoCamera.h"
-#import "GPUImageStillCamera.h"
-#import "GPUImageMovie.h"
 #import "GPUImagePicture.h"
 #import "GPUImageRawDataInput.h"
 #import "GPUImageRawDataOutput.h"
 #import "GPUImageMovieWriter.h"
-#import "GPUImageFilterPipeline.h"
 #import "GPUImageTextureOutput.h"
 #import "GPUImageFilterGroup.h"
 #import "GPUImageTextureInput.h"
-#import "GPUImageUIElement.h"
 #import "GPUImageBuffer.h"
 #import "GPUImageFramebuffer.h"
 #import "GPUImageFramebufferCache.h"
+
+#if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+#import "GPUImageVideoCamera.h"
+#import "GPUImageStillCamera.h"
+#import "GPUImageMovie.h"
+#import "GPUImageFilterPipeline.h"
+#import "GPUImageUIElement.h"
+#endif
 
 // Filters
 #import "GPUImageFilter.h"
