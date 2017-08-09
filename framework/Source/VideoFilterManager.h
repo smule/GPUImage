db7fe45ad2175994cbaffd358e44b40395f47b8e
Kyle Donnelly <kyle.donnelly@smule.com>
2017-08-08 21:10:43 -0700

    SWF-16632: Simplify VideoFilterManager for better encapsulation

--- framework/Source/VideoFilterManager.h
+++ framework/Source/VideoFilterManager.h
@@ -10,54 +10,10 @@
 #import "GPUImageFilterGallery.h"
 @class GPUImageFilterGroup;
 
-extern NSString * const kAirbrushFilterIdentifier;
-
-typedef NS_ENUM(NSInteger, VideoFilterType) {
-    VideoFilterTypeUnknown,
-    
-    VideoFilterTypeNormal,
-    VideoFilterTypeSepia,
-    VideoFilterTypeBlackWhite,
-    VideoFilterTypeVintage,
-    VideoFilterTypeSelfie,
-    VideoFilterTypeFightClub
-};
-
-// todo: delete
-// Transient class where it's only purposes is to store the filter list and vip filter's list
-@interface VideoFilterVariables : NSObject
-
-+ (VideoFilterVariables *)sharedInstance;
-// The names of the filters as strings.
-@property (nonatomic, strong) NSArray *filterList;
-// The name of the VIP filters as strings.
-@property (nonatomic, strong) NSArray *vipFilters;
-
-@end
 
 @interface VideoFilterManager : NSObject
 
 + (VideoFilterManager *)sharedInstance;
-
-// Filter name to send to server and map to localized string
-- (NSString *)filterNameForType:(VideoFilterType)videoFilterType;
-
-// Check if fiter at specific index is vip
-- (BOOL)isVIPOnlyFilter:(VideoFilterType)videoFilterType;
-
-// The names of the filters as strings.
-@property (nonatomic, strong) NSArray *filterList;
-// The name of the VIP filters as strings.
-@property (nonatomic, strong) NSArray *vipFilters;
-
-- (GPUImageOutput<GPUImageInput> *)filterWithFilterNames:(NSArray<NSString *> *)filterNames;
-
-// Old deprecated API using VideoFilterType.
-// TODO: Remove once we have switched over to the
-// filterWithVideoStyle:colorFilter:airbrushFilterType: method.
-- (GPUImageOutput<GPUImageInput> *)filterWithType:(VideoFilterType)videoFilterType
-                               airbrushFilterType:(AirbrushFilterType)airbrushFilterType;
-
 
 - (GPUImageOutput<GPUImageInput> *)filterWithVideoStyle:(ALYCEVideoStyle)videoStyle
                                             colorFilter:(ALYCEColorFilter)colorFilter
--- framework/Source/VideoFilterManager.m
+++ framework/Source/VideoFilterManager.m
@@ -13,25 +13,10 @@
 #import "MaskBlendFilter.h"
 #import "ScaleFilter.h"
 
-NSString * const kAirbrushFilterIdentifier = @"airbrush";
 
 @interface VideoFilterManager ()
 
 @property (nonatomic, strong) GPUImageFilterGallery *filterGallery;
-
-@end
-
-@implementation VideoFilterVariables
-
-+ (VideoFilterVariables *)sharedInstance {
-    static VideoFilterVariables *shared = nil;
-    static dispatch_once_t onceToken;
-    
-    dispatch_once(&onceToken, ^{
-        shared = [[VideoFilterVariables alloc] init];
-    });
-    return shared;
-}
 
 @end
 
@@ -45,178 +30,25 @@ NSString * const kAirbrushFilterIdentifier = @"airbrush";
 
     dispatch_once(&onceToken, ^{
         shared = [[VideoFilterManager alloc] init];
-        shared.filterList = [VideoFilterVariables sharedInstance].filterList;
-        shared.vipFilters = [VideoFilterVariables sharedInstance].vipFilters;
     });
     
     return shared;
 }
 
-#pragma mark - properties
-
-- (void)setVipFilters:(NSArray *)vipFilters {
-    if (_vipFilters == vipFilters) {
-        return;
+- (instancetype)init
+{
+    if (self = [super init]) {
+        [self setupGPUFilters];
     }
-
-    if ([vipFilters.firstObject isKindOfClass:[NSString class]]) {
-        NSMutableArray *filterArray = [NSMutableArray array];
-
-        for (NSString *filterName in vipFilters) {
-            VideoFilterType type = [self filterTypeForName:filterName];
-            if ((NSInteger)type >= 0) {
-                [filterArray addObject:@(type)];
-            }
-        }
-
-        _vipFilters = filterArray;
-    } else {
-        NSLog(@"vip filter list object type is not supported!");
-        _vipFilters = @[];
-        return;
-    }
-}
-
-- (void)setFilterList:(NSArray *)filterList {
-    if (_filterList == filterList) {
-        return;
-    }
-
-    // We'll only accept
-    if ([filterList.firstObject isKindOfClass:[NSString class]]) {
-        NSMutableArray *filterArray = [NSMutableArray array];
-
-        for (NSString *filterName in filterList) {
-            VideoFilterType type = [self filterTypeForName:filterName];
-            if ((NSInteger)type >= 0) {
-                [filterArray addObject:@(type)];
-            }
-        }
-
-        _filterList = filterArray;
-    } else {
-        NSLog(@"filter order list object type is not supported! normal will be only filter available");
-        _filterList = @[@(VideoFilterTypeNormal)];
-        return;
-    }
-
-    [self setupGPUFilters];
+    
+    return self;
 }
 
 - (void)setupGPUFilters {
     self.filterGallery = [GPUImageFilterGallery sharedInstance];
 }
 
-#pragma mark - private methods
-
-- (void)configureGalleryForVideoFilterType:(VideoFilterType)videoFilterType {
-    if (videoFilterType == VideoFilterTypeNormal) {
-        self.filterGallery.videoStyle = ALYCEVideoStyleClassic;
-        self.filterGallery.colorFilter = ALYCEColorFilterNone;
-    } else if (videoFilterType == VideoFilterTypeSepia) {
-        self.filterGallery.videoStyle = ALYCEVideoStyleClassic;
-        self.filterGallery.colorFilter = ALYCEColorFilterSepia;
-    } else if (videoFilterType == VideoFilterTypeBlackWhite) {
-        self.filterGallery.videoStyle = ALYCEVideoStyleClassic;
-        self.filterGallery.colorFilter = ALYCEColorFilterBlackAndWhite;
-    } else if (videoFilterType == VideoFilterTypeVintage) {
-        self.filterGallery.videoStyle = ALYCEVideoStyleOslo;
-        self.filterGallery.colorFilter = ALYCEColorFilterVibrant;
-    } else if (videoFilterType == VideoFilterTypeSelfie) {
-        self.filterGallery.videoStyle = ALYCEVideoStyleRio;
-        self.filterGallery.colorFilter = ALYCEColorFilterSoft;
-    } else if (videoFilterType == VideoFilterTypeFightClub) {
-        self.filterGallery.videoStyle = ALYCEVideoStylePetra;
-        self.filterGallery.colorFilter = ALYCEColorFilterMono;
-    } else {
-        NSLog(@"Unrecognized video filter type");
-    }
-}
-
-
 #pragma mark - public methods
-
-- (NSString *)filterNameForType:(VideoFilterType)videoFilterType {
-    switch (videoFilterType) {
-    case VideoFilterTypeNormal:
-        return @"normal";
-        break;
-    case VideoFilterTypeSepia:
-        return @"sepia";
-        break;
-    case VideoFilterTypeBlackWhite:
-        return @"bw";
-        break;
-    case VideoFilterTypeVintage:
-        return @"vintge";
-        break;
-    case VideoFilterTypeSelfie:
-        return @"selfie";
-        break;
-    case VideoFilterTypeFightClub:
-        return @"fight";
-        break;
-    default:
-        return @"normal";
-        break;
-    }
-}
-
-- (BOOL)isVIPOnlyFilter:(VideoFilterType)videoFilterType {
-    return [self.vipFilters containsObject:@(videoFilterType)];
-}
-
-- (VideoFilterType)filterTypeForName:(NSString *)videoFilterName {
-    NSDictionary *filterNameTypeMap = @{
-        @"normal": @(VideoFilterTypeNormal),
-        @"sepia": @(VideoFilterTypeSepia),
-        @"selfie": @(VideoFilterTypeSelfie),
-        @"fight": @(VideoFilterTypeFightClub),
-        @"vintage": @(VideoFilterTypeVintage),
-        @"bw": @(VideoFilterTypeBlackWhite)
-    };
-
-    NSNumber *typeNum = filterNameTypeMap[videoFilterName];
-    return (VideoFilterType)typeNum.integerValue;
-}
-
-- (GPUImageOutput<GPUImageInput> *)filterWithFilterNames:(NSArray<NSString *> *)filterNames {
-    
-    AirbrushFilterType airbrushFilterType = [filterNames containsObject:kAirbrushFilterIdentifier] ? AirbrushFilterTypeComplex : AirbrushFilterTypeNone;
-    NSString *lensFilterName = [filterNames lastObject];
-    
-    GPUImageOutput<GPUImageInput> *filter = nil;
-    
-    for (int i = 0; i < self.filterList.count; i++) {
-        VideoFilterType type = [self.filterList[i] integerValue];
-        NSString *name = [self filterNameForType:type];
-        
-        if ([name isEqualToString:lensFilterName]) {
-            filter = [self filterWithType:type airbrushFilterType:airbrushFilterType];
-            break;
-        }
-    }
-    
-    if (!filter)
-    {
-        // default (normal. no FX).
-        filter = [self filterWithType:VideoFilterTypeNormal airbrushFilterType:airbrushFilterType];
-    }
-    
-    [filter removeAllTargets];
-    
-    return filter;
-}
-
-- (GPUImageOutput<GPUImageInput> *)filterWithType:(VideoFilterType)videoFilterType
-                               airbrushFilterType:(AirbrushFilterType)airbrushFilterType {
-    runSynchronouslyOnVideoProcessingQueue(^{
-        [self.filterGallery setInputRotation:kGPUImageNoRotation atIndex:0];
-        [self configureGalleryForVideoFilterType:videoFilterType];
-        self.filterGallery.airbrushFilterType = airbrushFilterType;
-    });
-    return self.filterGallery;
-}
 
 - (GPUImageOutput<GPUImageInput> *)filterWithVideoStyle:(ALYCEVideoStyle)videoStyle
                                             colorFilter:(ALYCEColorFilter)colorFilter
